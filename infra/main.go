e60cdee3649980c029d4eb66803390dbbdb7f988
Gary Bowers <gary@bowers1.com>
2023-03-30 13:33:01 +0100

    Adding in component for GKE

--- infra/main.go
+++ infra/main.go
@@ -18,12 +18,19 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/compute"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/organizations"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/projects"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/serviceaccount"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+type ContainerClusterState struct {
+	pulumi.ResourceState
+}
+
+type ContainerClusterArgs struct{}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
@@ -67,7 +74,7 @@ func main() {
 		var project *organizations.Project
 		// Create the project using our credentials from the above provider configuration
 		project, err = organizations.NewProject(ctx, "mcg-demo", &organizations.ProjectArgs{
-			ProjectId:         pulumi.String("mcg-demo-h38hr2"),
+			ProjectId:         pulumi.String("mcg-demo-h38hr3"),
 			FolderId:          folder.Name,
 			AutoCreateNetwork: pulumi.Bool(false),
 			BillingAccount:    pulumi.String(billingAccount),
@@ -76,6 +83,7 @@ func main() {
 			return err
 		}
 
+		// Enable services on the account
 		services := []string{
 			"container.googleapis.com",
 			"artifactregistry.googleapis.com",
@@ -92,6 +100,71 @@ func main() {
 			}
 		}
 
+		// Create the VPC
+		vpc, err := compute.NewNetwork(ctx, "vpc", &compute.NetworkArgs{
+			AutoCreateSubnetworks: pulumi.Bool(false),
+			Project:               project.ProjectId,
+			RoutingMode:           pulumi.String("GLOBAL"),
+		})
+		if err != nil {
+			return err
+		}
+
+		regions := []string{
+			"europe-west1",
+			"us-central1",
+		}
+
+		// Create the required subnetworks
+		for i, region := range regions {
+			_, err := compute.NewSubnetwork(ctx, fmt.Sprintf("sn-%d-%s", i, region), &compute.SubnetworkArgs{
+				Project:               project.ProjectId,
+				Network:               vpc.SelfLink,
+				Region:                pulumi.String(region),
+				IpCidrRange:           pulumi.String(fmt.Sprintf("10.%d.0.0/16", i)),
+				PrivateIpGoogleAccess: pulumi.Bool(true),
+			})
+			if err != nil {
+				return err
+			}
+		}
+
+		// Create a NAT Router
+		for i, region := range regions {
+			router, err := compute.NewRouter(ctx, fmt.Sprintf("rtr-%d-%s", i, region), &compute.RouterArgs{
+				Project: project.ProjectId,
+				Region:  pulumi.String(region),
+				Network: vpc.SelfLink,
+				Bgp: &compute.RouterBgpArgs{
+					Asn: pulumi.Int(64514),
+				},
+			})
+			if err != nil {
+				return err
+			}
+			_, err = compute.NewRouterNat(ctx, fmt.Sprintf("nat-gw-%d-%s", i, region), &compute.RouterNatArgs{
+				Project:                       project.ProjectId,
+				Region:                        pulumi.String(region),
+				Router:                        router.Name,
+				NatIpAllocateOption:           pulumi.String("AUTO_ONLY"),
+				SourceSubnetworkIpRangesToNat: pulumi.String("ALL_SUBNETWORKS_ALL_IP_RANGES"),
+				LogConfig: &compute.RouterNatLogConfigArgs{
+					Enable: pulumi.Bool(true),
+					Filter: pulumi.String("ERRORS_ONLY"),
+				},
+			})
+		}
+
 		return nil
 	})
+}
+
+func NewContainerCluster(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*ContainerClusterState, error) {
+	containerCluster := &ContainerClusterState{}
+	err := ctx.RegisterComponentResource("pkg:google:ContainerCluster", name, containerCluster, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return containerCluster, nil
 }
